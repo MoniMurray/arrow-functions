a0e81fdf84a29a340d8e466af84cfaf8e7330a47
Monica Murray <Monica.rickerby@gmail.com>
2023-01-04 23:01:17 +0000

    Initial arrow functions commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,44 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a,b) {
+//     // code block here
+//     return a + b;
+// }
 
-// Arrow Function With Parameters
+// call the function
+// let sum = addTwoNumbers(3,5);
+// console.log(sum);
 
+// Arrow Function With Parameters. Define the function using "constant" plus the function name and set it equal to a set of parentheses
+// that contains our parameters, then a "fat arrow" pointing to our opening curly braces with the function body, then close the curly braces.
+// The function is called in the exact same way.
+
+const addTwoNumbers = (a,b) => {
+    // code block here
+    return a + b;
+}
+
+// // call the function
+let sum = addTwoNumbers(3,5);
+console.log(sum);
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a,b) => (a + b);
+
+let sums = addTwoNumbers2(6,6);
+console.log(sums);
+
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething("I learned a new thing");
+const sayHello = () => console.log("hello");
+sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string!
+    </p>`
+)
+console.log(returnMultipleLines());
